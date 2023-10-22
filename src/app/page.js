3c88bb005b497229c26e397e67d4cbdb0f67e49d
Arash Zakeresfahani <azakeres@iu.edu>
2023-10-22 16:18:53 -0400

    nav bar

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,21 @@
-import Image from 'next/image'
+import { promises as fs } from 'fs';
+import NavBar from './NavBar';
 
-export default function Home() {
+export default async function Home() {
+
+  const file = await fs.readFile(process.cwd() + '/public/assets/data.json', 'utf8');
+  const data = JSON.parse(file);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main >
+      <NavBar />
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <h1>hello world!</h1>
+        {
+          data.map((item, index) => (
+            <h6 key={index}>{item.country}</h6>
+          ))
+        }
       </div>
     </main>
   )
